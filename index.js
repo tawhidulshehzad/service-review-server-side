67a03ecb337e961d81b34b49aada71f0e981b932
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-09 23:37:07 +0600

    data distructure style

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
-
+    // data taking
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
