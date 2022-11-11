58bbce055ccd8a55b4215c18230743f0c5cebe9f
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-11 20:33:16 +0600

    sorting updated

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function run() {
     // all services send
     app.get("/services", async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query);
+      const cursor = serviceCollection.find(query, { sort: { time: -1 } });
       const services = await cursor.limit(3).toArray();
       res.send(services);
     });
